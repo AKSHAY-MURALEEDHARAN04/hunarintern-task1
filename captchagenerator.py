e0a12e6e00bd8e6b9f0b5e26fd048264d500d05b
AKSHAY-MURALEEDHARAN04 <161454387+AKSHAY-MURALEEDHARAN04@users.noreply.github.com>
2024-03-10 22:06:07 +0530

    Create captchagenerator.py

---
+++ captchagenerator.py
@@ -0,0 +1,9 @@
+from random import choice
+
+A="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+
+captcha =""
+for i in range(5):
+    captcha+=choice(A)
+
+    print("Your captcha :",captcha)
